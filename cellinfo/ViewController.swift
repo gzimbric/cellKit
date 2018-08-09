457041c4ff836474e51f1c3b46c358eaa58c40e6
gzimbric <gabe@zimbri.cc>
2018-08-09 02:47:30 -0500

    init codebase upload

--- cellinfo/ViewController.swift
+++ cellinfo/ViewController.swift
@@ -7,14 +7,32 @@
 //
 
 import UIKit
+import CoreTelephony
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var carrierLabel: UILabel!
+    @IBOutlet weak var radioLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Setup view
+        navigationController?.navigationBar.prefersLargeTitles = true
+        // Get telephony info
+        let telefonyInfo = CTTelephonyNetworkInfo()
+        // Get carrier and display in view
+        let carrier = telefonyInfo.subscriberCellularProvider
+        print(carrier!.carrierName!)
+        self.carrierLabel.text = String(carrier!.carrierName!)
+        // Get radio state and display in view
+        if let radioAccessTechnology = telefonyInfo.currentRadioAccessTechnology{
+            switch radioAccessTechnology {
+            case CTRadioAccessTechnologyLTE: self.radioLabel.text = "LTE (4G)"; print("LTE (4G)")
+            case CTRadioAccessTechnologyWCDMA: self.radioLabel.text = "WCDMA (3G)"; print("WCDMA (3G)")
+            case CTRadioAccessTechnologyEdge: self.radioLabel.text = "EDGE (2G)"; print("EDGE (2G)")
+            case CTRadioAccessTechnologyeHRPD: self.radioLabel.text = "eHRPD (3G)"; print("eHRPD (3G)")
+            default: self.radioLabel.text = "Unknown"; print("Unknown")
+            }
+        }
     }
-
-
 }
-
