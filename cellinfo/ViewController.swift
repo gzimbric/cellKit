44ecb189e93108bd162d9bf1747ba623125c7f63
gzimbric <gabe@zimbri.cc>
2018-08-09 01:38:07 -0500

    Initial Commit

---
+++ cellinfo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cellinfo
+//
+//  Created by Gabe Zimbric on 8/9/18.
+//  Copyright © 2018 Gabe Zimbric. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
